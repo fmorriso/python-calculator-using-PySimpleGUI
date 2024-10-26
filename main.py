cf0a82fdd52caf669fedef6bf36a20fb146cb2b5
Fred Morrison <frederick.morrison@outlook.com>
2024-10-26 09:16:37 -0600

    use bold font

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def get_python_version() -> str:
 # Press the green button in the gutter to run the script.
 def verify_yes_no_popup_works():
     resp = sg.popup_yes_no( 'Do you want to exit?', title='Yes/No',
-                            font='"Courier New" 20', keep_on_top=True, text_color='white',
+                            font='"Courier New" 20 bold', keep_on_top=True, text_color='white',
                             button_color=('#00008B','white'), background_color='#00008B')
     print(f'{type(resp)=}, {resp=}')
     # if sg.popup_yes_no('Do you want to exit?') == 'Yes':
