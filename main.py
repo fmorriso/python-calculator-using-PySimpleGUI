57d2371feb14dcc3ac2041ff72c93804f76d3de5
Fred Morrison <frederick.morrison@outlook.com>
2024-10-26 08:45:55 -0600

    add Yes/No dialog test

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import sys, platform
-
+import PySimpleGUI as sg
 
 def get_python_version() -> str:
     return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
 
 
 # Press the green button in the gutter to run the script.
+def verify_yes_no_popup_works():
+    resp = sg.popup_yes_no( 'Do you want to exit?', title='Yes/No',
+                            font='"Courier New" 20', keep_on_top=True, text_color='white',
+                            button_color='#000088')
+    print(f'{type(resp)=}, {resp=}')
+    # if sg.popup_yes_no('Do you want to exit?') == 'Yes':
+    #     exit()
+
 def main():
     msg = f'Python version: {get_python_version()} on {platform.system()} {platform.release()}'
     print(msg)
+    verify_yes_no_popup_works()
 
 
 if __name__ == '__main__':
