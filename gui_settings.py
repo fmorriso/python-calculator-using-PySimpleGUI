1eca1927faf8e96ab4ff34111d08099eb589b428
Fred Morrison <frederick.morrison@outlook.com>
2024-10-26 14:10:45 -0600

    Start of GuiSettings enumeration

---
+++ gui_settings.py
@@ -0,0 +1,8 @@
+from enum import StrEnum, auto
+
+class GuiSettings(StrEnum):
+    background_color = '#00008B',
+    button_color_foreground = '#00008B',
+    button_color_background = '#FFFFFF',
+    font = '"Courier New" 20 bold',
+    text_color='#FFFFFF'
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import sys, platform
 import PySimpleGUI as sg
 
+from gui_settings import GuiSettings
+
+
 def get_python_version() -> str:
     return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
 
 
 def verify_yes_no_popup_works():
     resp = sg.popup_yes_no( 'Do you want to exit?', title='Yes/No',
-                            font='"Courier New" 20 bold', keep_on_top=True, text_color='white',
-                            button_color=('#00008B','white'), background_color='#00008B')
+                            font=GuiSettings.font, keep_on_top=True,
+                            text_color=GuiSettings.text_color,
+                            button_color=(GuiSettings.button_color_foreground, GuiSettings.button_color_background),
+                            background_color=GuiSettings.background_color)
     print(f'{type(resp)=}, {resp=}')
     # if sg.popup_yes_no('Do you want to exit?') == 'Yes':
     #     exit()
