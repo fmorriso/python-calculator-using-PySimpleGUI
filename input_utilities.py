beb784d818a6a813c5b6fe42cf8ae45e3b13fc59
Fred Morrison <frederick.morrison@outlook.com>
2024-10-26 20:30:42 -0600

    improved get_single_choice

--- input_utilities.py
+++ input_utilities.py
@@ -45,13 +45,16 @@ class InputUtils:
         layout = [
             [sg.Text(prompt, text_color=GuiSettings.text_color, background_color=GuiSettings.background_color)],
             [sg.Combo(values=choices, readonly=True, key=GuiSettings.combo_selection_key, default_value=choices[0])],
-            [sg.Button(GuiSettings.button_OK, button_color=(GuiSettings.button_color_foreground, GuiSettings.button_color_background))],
-            [sg.Button(GuiSettings.button_CANCEL, button_color=(GuiSettings.button_color_foreground, GuiSettings.button_color_background))]
+            [
+                sg.Button(GuiSettings.button_OK, button_color=(GuiSettings.button_color_foreground, GuiSettings.button_color_background)),
+                sg.Button(GuiSettings.button_CANCEL, button_color=(GuiSettings.button_color_foreground, GuiSettings.button_color_background))
+             ]
         ]
         window = sg.Window(title, layout,
                            background_color=GuiSettings.background_color,
                            button_color=(GuiSettings.button_color_foreground, GuiSettings.button_color_background),
-                           font=GuiSettings.font)
+                           font=GuiSettings.font,
+                           auto_size_text=True, size=(400, 200))
         choice = ''
         while True:
             event, values = window.read()
