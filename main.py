73b20b47daa3cca1a97637390fba14eae4b09f4e
Fred Morrison <frederick.morrison@outlook.com>
2024-10-28 08:15:53 -0600

    Use enumeration for valid mathematical operations

--- main.py
+++ main.py
@@ -13,23 +13,23 @@ def get_python_version() -> str:
 
 def verify_yes_no_popup_works():
     """Verify that yes/no question utility works correctly."""
-    resp: bool = InputUtils.get_yesno_response('Exit?','Do you want to quit?')
+    resp: bool = InputUtils.get_yesno_response('Exit?', 'Do you want to quit?')
     print(f'{type(resp)=}, {resp=}')
 
 
 def verify_get_single_choice_from_list_works():
-    choices: list[str] = ['Apple','Banana','Orange']
+    choices: list[str] = ['Apple', 'Banana', 'Orange']
     choice = InputUtils.get_single_choice('Fruit Choice', 'Pick a fruit', choices)
     print(f'{type(choice)=}, {choice=}')
 
 
 def verify_get_floating_point_number_works():
-    n: float = InputUtils.get_floating_point_number('Deposit Amount','Enter deposit amount')
+    n: float = InputUtils.get_floating_point_number('Deposit Amount', 'Enter deposit amount')
     print(f'{type(n)=}, {n=}')
 
 
 def verify_get_decimal_number_works():
-    n: decimal.Decimal = InputUtils.get_decimal_number(title='Radius', prompt =  'Enter radius')
+    n: decimal.Decimal = InputUtils.get_decimal_number(title = 'Radius', prompt = 'Enter radius')
     print(f'{type(n)=}, {n=}, {n*2=}')
 
 
@@ -37,10 +37,13 @@ def verify_get_whole_number_works():
     n: int = InputUtils.get_whole_number(title = 'Eggs', prompt = 'Enter number of eggs')
     print(f'{type(n)=}, {n=}, {n*2=}')
 
+
 def get_operation() -> str:
     choices: list[str] = MathOperation.get_operations_list()
     opr = InputUtils.get_single_choice('Operation', 'Select an operation', choices)
     return opr
+
+
 def perform_operation(num1, num2, operation):
     match operation:
         case MathOperation.addition:
@@ -54,14 +57,16 @@ def perform_operation(num1, num2, operation):
         case MathOperation.modulo:
             return num1 % num2
 
+
 def perform_one_calculation():
     num1 = InputUtils.get_decimal_number('First', 'Enter the first number:')
     num2 = InputUtils.get_decimal_number('Second', 'Enter the second number:')
     operation = get_operation()
     result = perform_operation(num1, num2, operation)
-    msg = f'{num1} {operation} {num2} = {result}'
+    msg = f'{num1} {operation[0]} {num2} = {result}'
     print(msg)
     OutputUtils.display_message(msg, 'Result')
+
 
 def main():
     msg = f'Python version: {get_python_version()} on {platform.system()} {platform.release()}'
@@ -79,6 +84,7 @@ def main():
     msg = 'Thank you for using my calculator'
     print(msg)
     OutputUtils.display_message(msg, 'Calculator')
+
 
 if __name__ == '__main__':
     main()
--- mathematical_operations.py
+++ mathematical_operations.py
@@ -11,5 +11,21 @@ class MathOperation(StrEnum):
     def get_operations_list():
         lst: list[str] = []
         for operator in MathOperation:
-            lst.append(operator.value)
+            lst.append(f'{operator.value} {operator.name}')
         return lst
+
+
+    def __str__(self):
+        match self.name:
+            case '+':
+                return '+ Addition'
+            case '-':
+                return '- Subtraction'
+            case '*':
+                return '* Multiplication'
+            case '/':
+                return '/ Division'
+            case '%':
+                return '% Modulo'
+            case _:
+                return 'Operation not found'
